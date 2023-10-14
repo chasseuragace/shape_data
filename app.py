442fff0e8977d5ddd085fc0110d97e2aaaa80977
Chasseur Agace <34769013+chasseuragace@users.noreply.github.com>
2023-10-14 20:35:52 +0545

    wih and without walk

--- app.py
+++ app.py
@@ -7,20 +7,23 @@ from shapely.geometry import Point
 app = Flask(__name__)
 
 # Define the directory paths for h1 and h2
-directory_paths = ["/h_data/h2", "/h_data/h1"]
+directory_paths = ["/app/h_data/h2", "/app/h_data/h1"]
 
-@app.route('/get_info', methods=['GET'])
-def get_info():
-    lat = float(request.args.get('lat'))
-    lon = float(request.args.get('lon'))
 
+def load_shapefiles(lat, lon, approach="os.walk"):
+    # Record the start time for query execution
+    start_time = time.time()
     result = []
-    available_directories = []
+
+    # Determine the current directory based on the chosen approach
+    if approach == "os.walk":
+        current_directory = os.getcwd()
+    else:
+        current_directory = None  # Set this to the correct directory
 
     # Iterate through both directories
     for directory_path in directory_paths:
         if os.path.exists(directory_path):
-            available_directories.append(directory_path)
             for i in range(4):  # Assuming you have new_wgs_0.shp to new_wgs_3.shp
                 shp_filename = f"hermes_NPL_new_wgs_{i}.shp"
                 shp_path = os.path.join(directory_path, shp_filename)
@@ -32,87 +35,74 @@ def get_info():
                     # Create a shapely Point object from the lat/lon coordinates
                     point = Point(lon, lat)
 
+                    
+
                     # Use the GeoDataFrame's "contains" method to find the administrative unit that contains the point
                     admin_unit = gdf[gdf.geometry.contains(point)]
 
-                    # If an administrative unit is found, extract the attributes and add them to the result
-                    if not admin_unit.empty:
-                        attrs = admin_unit.iloc[0].to_dict()
-                        result.append(attrs)
+                    # Record the end time for query execution
+                    end_time = time.time()
+                    query_time = end_time - start_time
 
-    accessible_directories = [dirpath for dirpath in directory_paths if os.path.exists(dirpath)]
+                    # Iterate through all the found administrative units and extract attributes
+                    for _, admin_unit in admin_unit.iterrows():
+                        attrs = admin_unit.to_dict()
 
-    response = {"result": result, "available_directories": accessible_directories}
+                        # Append the keys and values (excluding "geometry") to the result list
+                        keys_values = {key: value for key, value in attrs.items() if key != "geometry"}
 
-    if result:
-        return jsonify(response)
-    else:
-        return jsonify({"message": "No administrative units found for the given coordinates.", "available_directories": available_directories}), 404
+                        # Include the filename where the data is found from
+                        keys_values["filename"] = shp_filename
 
+                        # Create a formattedString based on available attributes
+                        formatted_string = []
 
+                        if "PALIKA" in attrs:
+                            formatted_string.append(attrs["PALIKA"])
+                        if "DISTRICT" in attrs:
+                            formatted_string.append(attrs["DISTRICT"])
+                        if "PR_NAME" in attrs:
+                            formatted_string.append(attrs["PR_NAME"])
+                        if "PROVINCE" in attrs and "PR_NAME" not in attrs:
+                            formatted_string.append(f"Province {attrs['PROVINCE']}")
+                        if "TYPE" in attrs:
+                            formatted_string.append(attrs["TYPE"])
+                        if "LOCAL" in attrs:
+                            formatted_string.append(attrs["LOCAL"])
 
-@app.route('/get_infos', methods=['GET'])
-def get_infos():
+                        # Construct the final formattedString
+                        keys_values["formattedString"] = ", ".join(formatted_string)
+                        keys_values["query_time"] = query_time
+
+                        result.append(keys_values)
+
+    return result
+
+@app.route('/get_info', methods=['GET'])
+def get_info():
     lat = float(request.args.get('lat'))
     lon = float(request.args.get('lon'))
 
-    result = []
+    result = load_shapefiles(lat, lon, approach="os.walk")
 
-    # Get the content of the current directory (container's working directory)
-    current_directory = os.getcwd()
+    if result:
+        return jsonify({"result":result})
+    else:
+        return jsonify({"message": "No administrative units found for the given coordinates."}), 404
 
-    # Use os.walk to traverse the directory tree
-    for root, dirs, files in os.walk(current_directory):
-        for file in files:
-            if file.endswith(".shp"):
-                shp_path = os.path.join(root, file)
 
-                # Load the Shapefile into a GeoDataFrame
-                gdf = gpd.read_file(shp_path)
+@app.route('/get_info_gpd', methods=['GET'])
+def get_info_gpd():
+    lat = float(request.args.get('lat'))
+    lon = float(request.args.get('lon'))
 
-                # Create a shapely Point object from the lat/lon coordinates
-                point = Point(lon, lat)
-
-                # Record the start time for query execution
-                start_time = time.time()
-
-                # Use the GeoDataFrame's "contains" method to find the administrative units that contain the point
-                admin_units = gdf[gdf.geometry.contains(point)]
-
-                # Record the end time for query execution
-                end_time = time.time()
-                query_time = end_time - start_time
-
-                # Iterate through all the found administrative units and extract attributes
-                for _, admin_unit in admin_units.iterrows():
-                    attrs = admin_unit.to_dict()
-                    # Append the keys and values (excluding "geometry") to the result list
-                    keys_values = {key: value for key, value in attrs.items() if key != "geometry"}
-                    # Include the filename where the data is found from
-                    keys_values["filename"] = file
-                    # Include the query time in seconds
-                    keys_values["query_time"] = query_time
-                    result.append(keys_values)
+    result = load_shapefiles(lat, lon, approach="gpd")
 
     if result:
         return jsonify(result)
     else:
         return jsonify({"message": "No administrative units found for the given coordinates."}), 404
 
-
-@app.route('/get_directory_list', methods=['GET'])
-def get_directory_list():
-    result = []
-    # Get the content of the current directory (container's working directory)
-    current_directory = os.getcwd()
-    current_directory_contents = os.listdir(current_directory)
-
-    # Add the current directory's contents to the result
-    result.append(f"Contents of current directory ({current_directory}):")
-    for item in current_directory_contents:
-        result.append(item)
-
-    return jsonify(result)
 
 
 if __name__ == '__main__':
